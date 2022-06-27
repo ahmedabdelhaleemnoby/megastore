e35b276668ee69df0accd8aa2148c9d7eeae0bb5
ahmedabdelahleem <90034764+ahmedabdelahleem@users.noreply.github.com>
2022-06-27 16:40:48 +0200

    new update30

--- inc/product.php
+++ inc/product.php
@@ -1,6 +1,6 @@
 <!-- Product Start -->
-<div class="container-xxl py-5">
-    <div class="container">
+<div class="container-xxl py-5 mt-5">
+    <div class="container py-4">
         <div class="row g-0 gx-5 align-items-end">
             <div class="col-lg-6">
                 <div class="section-header text-start mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
@@ -40,8 +40,11 @@
 
                 <div class="row g-4">
                     <?php
-                    $select_pro = mysqli_query($con, "SELECT * FROM product WHERE category= 1");
-
+                    if (basename($_SERVER['PHP_SELF']) == "products.php") {
+                        $select_pro = mysqli_query($con, "SELECT * FROM product ORDER BY id DESC");
+                    } else {
+                        $select_pro = mysqli_query($con, "SELECT * FROM product ORDER BY id DESC  LIMIT 8");
+                    };
                     while ($product = mysqli_fetch_array($select_pro)) {
 
                     ?>
@@ -72,14 +75,15 @@
                     };
                     ?>
                     <div class="col-12 text-center wow fadeInUp" data-wow-delay="0.1s">
-                        <a class="btn btn-primary rounded-pill py-3 px-5" href="">Browse More Products</a>
+                        <a class="btn btn-primary rounded-pill py-3 px-5" href="products.php">Browse More Products </a>
                     </div>
                 </div>
             </div>
-            <div id="tab-2" class="tab-pane fade show p-0">
+            <div id="tab-2" class="tab-pane fade show p-0 ">
+
                 <div class="row g-4">
                     <?php
-                    $select_pro = mysqli_query($con, "SELECT * FROM product WHERE category= 3");
+                    $select_pro = mysqli_query($con, "SELECT * FROM product WHERE category= 3 LIMIT 8");
 
                     while ($product = mysqli_fetch_array($select_pro)) {
 
@@ -111,14 +115,14 @@
                     };
                     ?>
                     <div class="col-12 text-center wow fadeInUp" data-wow-delay="0.1s">
-                        <a class="btn btn-primary rounded-pill py-3 px-5" href="">Browse More Products</a>
+                        <a class="btn btn-primary rounded-pill py-3 px-5" href="products.php">Browse More Products</a>
                     </div>
                 </div>
             </div>
             <div id="tab-3" class="tab-pane fade show p-0">
                 <div class="row g-4">
                     <?php
-                    $select_pro = mysqli_query($con, "SELECT * FROM product WHERE category= 4");
+                    $select_pro = mysqli_query($con, "SELECT * FROM product WHERE category= 4 LIMIT 8");
 
                     while ($product = mysqli_fetch_array($select_pro)) {
 
@@ -150,7 +154,46 @@
                     };
                     ?>
                     <div class="col-12 text-center wow fadeInUp" data-wow-delay="0.1s">
-                        <a class="btn btn-primary rounded-pill py-3 px-5" href="">Browse More Products</a>
+                        <a class="btn btn-primary rounded-pill py-3 px-5" href="products.php">Browse More Products</a>
+                    </div>
+                </div>
+            </div>
+            <div id="tab-4" class="tab-pane fade show p-0">
+                <div class="row g-4">
+                    <?php
+                    $select_pro = mysqli_query($con, "SELECT * FROM product WHERE category= 5 LIMIT 8");
+
+                    while ($product = mysqli_fetch_array($select_pro)) {
+
+                    ?>
+                        <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                            <div class="product-item">
+                                <div class="position-relative bg-light overflow-hidden">
+                                    <img class="img-fluid w-100" src="admin/inc/Products/upload/<?= $product['images'] ?>" alt="">
+                                    <div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
+                                </div>
+
+                                <div class="text-center p-4">
+                                    <a class="d-block h5 mb-2" href=""><?= $product['name'] ?></a>
+                                    <span class="text-primary me-1">$<?= $product['price'] - $product['discount']; ?>.00</span>
+                                    <span class="text-body text-decoration-line-through">$<?= $product['price'] ?></span>
+                                </div>
+                                <div class="d-flex border-top">
+                                    <small class="w-50 text-center border-end py-2">
+                                        <a class="text-body" href="<?= $product['links-item'] ?>"><i class="fa fa-eye text-primary me-2"></i><?= $product['description'] ?></a>
+                                    </small>
+                                    <small class="w-50 text-center py-2">
+                                        <a class="text-body" href=""><i class="fa fa-shopping-bag text-primary me-2"></i>Add to cart</a>
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php
+                    };
+                    ?>
+                    <div class="col-12 text-center wow fadeInUp" data-wow-delay="0.1s">
+                        <a class="btn btn-primary rounded-pill py-3 px-5" href="products.php">Browse More Products</a>
                     </div>
                 </div>
             </div>
---
+++ products.php
@@ -0,0 +1,26 @@
+<?php
+include("inc/head.php");
+?>
+</head>
+
+<body>
+
+
+
+    <?php
+    include("inc/header.php");
+    ?>
+
+
+
+    <?php
+    include("inc/product.php")
+    ?>
+
+
+    <?php
+    include("inc/footer.php")
+    ?>
+</body>
+
+</html>
